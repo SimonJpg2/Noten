244b41a632bf0a8e1481d5048d2b8a411b821b0d
SimonJpg <simonbalcke3@gmail.com>
2023-01-28 12:45:41 +0100

    feat(Backend): add LoginController and User class

--- src/main/java/de/simonjpg/noten/Backend/LoginControllerImplementation.java
+++ src/main/java/de/simonjpg/noten/Backend/LoginControllerImplementation.java
@@ -1,35 +1,241 @@
 package de.simonjpg.noten.Backend;
 
+import de.simonjpg.noten.Backend.DB.Database;
 import de.simonjpg.noten.Backend.DB.LoginController;
+import de.simonjpg.noten.Backend.Security.SHA256;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class LoginControllerImplementation implements LoginController {
+import static de.simonjpg.noten.Backend.Security.BadCharacters.FORBIDDEN;
 
-    // TODO implement method logic
+/**
+ * Class LoginControllerImplementation.
+ *
+ * <p>
+ *     This class contains logic to store data of the user in a database.
+ * </p>
+ *
+ * @version 1.0
+ * @since 28.1.2023
+ * @author SimonJpg
+ * @see de.simonjpg.noten.Backend.DB.LoginController
+ */
+
+public class LoginControllerImplementation implements LoginController {
+    private final Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+    private final SHA256 sha256;
+
+    /**
+     * Constructor LoginControllerImplementation.
+     *
+     * <p>
+     *     Creates connection to database login.
+     * </p>
+     *
+     * @since 28.1.2023
+     * @author SimonJpg
+     */
+
+    public LoginControllerImplementation() {
+        this.connection = new Database().init("jdbc:mysql://localhost:3306/login");
+        sha256 = new SHA256();
+    }
+
+    /**
+     * Constructor LoginControllerImplementation.
+     *
+     * <p>
+     *     Creates connection to database test.
+     * </p>
+     *
+     * @since 28.1.2023
+     * @author SimonJpg
+     */
+
+    public LoginControllerImplementation(String url) {
+        this.connection = new Database().init(url);
+        sha256 = new SHA256();
+    }
+
+    // TODO add testcases
+    // TODO connect to frontend
+
+    /**
+     * Method select.
+     *
+     * <p>
+     *     selects all users from database login
+     * </p>
+     *
+     * @return an {@link java.util.List} instance.
+     */
 
     @Override
     public List<User> select() {
-        return null;
+        try {
+            preparedStatement = connection.prepareStatement("SELECT * FROM users");
+            resultSet = preparedStatement.executeQuery();
+
+            List<User> users = new ArrayList<>();
+
+            while (resultSet.next()) {
+                users.add(
+                        new User(
+                                resultSet.getInt("id"),
+                                resultSet.getString("username"),
+                                resultSet.getString("email"),
+                                resultSet.getString("password")
+                        )
+                );
+            }
+            resultSet.close();
+            preparedStatement.close();
+            return users;
+        } catch (SQLException | NullPointerException e) {
+            System.err.printf("selection failed: %s", e.getMessage());
+            preparedStatement = null;
+            resultSet = null;
+            return new ArrayList<>();
+        }
     }
+
+    /**
+     * Method selectById.
+     *
+     * <p>
+     *     Selects a user by id.
+     * </p>
+     *
+     * @param id an {@link java.lang.Integer} the id.
+     * @return a {@link de.simonjpg.noten.Backend.User} instance.
+     */
 
     @Override
     public User selectById(int id) {
-        return null;
+        try {
+            preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            User user = new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("username"),
+                    resultSet.getString("email"),
+                    resultSet.getString("password")
+            );
+            resultSet.close();
+            preparedStatement.close();
+            return user;
+        } catch (SQLException | NullPointerException e) {
+            System.err.printf("selectById failed: %s", e.getMessage());
+            preparedStatement = null;
+            resultSet = null;
+            return new User();
+        }
     }
+
+    /**
+     * Method create.
+     *
+     * <p>
+     *     Creates new user in Database.
+     *     Hashes the email and password before it is stored in the database.
+     * </p>
+     *
+     * @param user the {@link de.simonjpg.noten.Backend.User} to be added.
+     * @return a {@link java.lang.Boolean} if succeeded.
+     */
 
     @Override
     public boolean create(User user) {
-        return false;
+        for (String str : FORBIDDEN) {
+            if (user.getUsername().contains(str) || user.getEmail().contains(str) || user.getPassword().contains(str)) {
+                System.err.println("Forbidden character, could lead to SQL Injection\n");
+                return false;
+            }
+        }
+
+        try {
+            preparedStatement = connection.prepareStatement("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, sha256.hash(user.getEmail()));
+            preparedStatement.setString(3, sha256.hash(user.getPassword()));
+            boolean success = preparedStatement.executeUpdate() > 0;
+            preparedStatement.close();
+            return success;
+        } catch (SQLException | RuntimeException e) {
+            System.err.printf("Insertion failed: %s", e.getMessage());
+            preparedStatement = null;
+            return false;
+        }
     }
+
+    /**
+     * Method updateById.
+     *
+     * <p>
+     *     Updates existing user entry by id.
+     *     Hashes the email and password before it is stored in the database.
+     * </p>
+     * @param id the {@link java.lang.Integer} id.
+     * @param user the updatet {@link de.simonjpg.noten.Backend.User}.
+     * @return a {@link java.lang.Boolean} if succeeded.
+     */
 
     @Override
     public boolean updateById(int id, User user) {
-        return false;
+        for (String str : FORBIDDEN) {
+            if (user.getUsername().contains(str) || user.getEmail().contains(str) || user.getPassword().contains(str)) {
+                System.err.println("Forbidden character, could lead to SQL Injection\n");
+                return false;
+            }
+        }
+
+        try {
+            preparedStatement = connection.prepareStatement("UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?");
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, sha256.hash(user.getEmail()));
+            preparedStatement.setString(3, sha256.hash(user.getPassword()));
+            preparedStatement.setInt(4, id);
+            boolean success = preparedStatement.executeUpdate() > 0;
+            preparedStatement.close();
+            return success;
+        } catch (SQLException | RuntimeException e) {
+            System.err.printf("UpdateById failed: %s", e.getMessage());
+            preparedStatement = null;
+            return false;
+        }
     }
+
+    /**
+     * Method deleteById.
+     *
+     * <p>
+     *     Deletes an existing entry by id.
+     * </p>
+     * @param id the {@link java.lang.Integer} id.
+     * @return a {@link java.lang.Boolean} if succeeded.
+     */
 
     @Override
     public boolean deleteById(int id) {
-        return false;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            boolean success = preparedStatement.executeUpdate() > 0;
+            preparedStatement.close();
+            return success;
+        } catch (SQLException | NullPointerException e) {
+            System.err.printf("DeleteById failed: %s", e.getMessage());
+            preparedStatement = null;
+            return false;
+        }
     }
 }
--- src/main/java/de/simonjpg/noten/Backend/User.java
+++ src/main/java/de/simonjpg/noten/Backend/User.java
@@ -1,10 +1,25 @@
 package de.simonjpg.noten.Backend;
 
+/**
+ * Class User.
+ *
+ * <p>
+ *     This class represents a user for this application.
+ * </p>
+ *
+ * @version 1.0
+ * @since 28.1.2023
+ * @author SimonJpg
+ */
+
 public class User {
     private int id;
     private String username;
     private String email;
     private String password;
+
+    public User() {
+    }
 
     public User(String username, String email, String password) {
         this.username = username;
