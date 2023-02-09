97836ee649bf830dc5515f657364a904bb13e8ea
SimonJpg <simonbalcke3@gmail.com>
2023-02-09 17:45:48 +0100

    feat(Test): add testcase

--- src/test/java/de/simonjpg/noten/Backend/Images/ImageHandlerTest.java
+++ src/test/java/de/simonjpg/noten/Backend/Images/ImageHandlerTest.java
@@ -1,4 +1,19 @@
+package de.simonjpg.noten.Backend.Images;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ImageHandlerTest {
-  
+    @Test
+    void testGetImageFromResource() {
+        // given
+        ImageHandler handler = new ImageHandler();
+        // when
+        Image given = handler.getImageFromResource("FrameImageIcon.jpg");
+        // then
+        assertNotNull(given);
+    }
 }
